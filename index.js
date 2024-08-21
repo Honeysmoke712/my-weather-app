3438d505f7d2c7589bd8a2ff49a925a1f5c0d182
Katie Payton <kbpayton91@gmail.com>
2024-08-21 06:14:10 -0500

    updated weather data

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
+
+
+function updateWeather(response) {
+
+    let temperatureElement = document.querySelector("#temp");
+    let temperature = (response.data.temperature.current);
+    temperatureElement.innerHTML = Math.round(temperature);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+}
+//API
+function searchCity(city) {
+   let apiKey = "0fo9f7cff9112b546tb3124acea4be3b";
+   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  
+axios.get(apiUrl).then(updateWeather);
+}
+
+//FORM
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+
+    searchCity(searchInput.value);
  }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+
 
 
